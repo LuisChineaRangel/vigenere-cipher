0dc8e1e86e8245b7ed4c7a5dd981833f81e220b1
IluzioDev <luischinearangel@gmail.com>
2023-02-03 11:05:16 +0000

    Detection of invalid characters

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -1,21 +1,34 @@
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+def char_in_alphabet(char):
+  if char in alphabet:
+    return True
+  return False
+
 def encrypt_char(plain_char, key_char):
+  if plain_char not in alphabet or key_char not in alphabet:
+    return False
   return alphabet[((alphabet.index(plain_char) + alphabet.index(key_char)) % len(alphabet))]
 
 def encrypt(plain_text, key):
   encrypted_text = str()
   for i in range(len(plain_text)):
+    if not char_in_alphabet(plain_text[i]):
+      return 'Text with invalid characters'
     encrypted_text += encrypt_char(plain_text[i], key[i % len(key)])
   return encrypted_text
 
 def decrypt_char(encripted_char, key_char):
+  if encripted_char not in alphabet or key_char not in alphabet:
+    return False
   return alphabet[((alphabet.index(encripted_char) - alphabet.index(key_char)) % len(alphabet))]
 
 def decrypt(encrypted_text, key):
   decrypted_text = str()
   for i in range(len(encrypted_text)):
+    if not char_in_alphabet(encrypted_text[i]):
+      return 'Text with invalid characters'
     decrypted_text += decrypt_char(encrypted_text[i], key[i % len(key)])
   return decrypted_text
 
